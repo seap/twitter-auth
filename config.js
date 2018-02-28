ad2e06420ce0a6cc46aabd57bf3f4354234453e4
seayang <ykudos@gmail.com>
2018-02-28 15:59:32 +0800

    update callback url to https as default

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const consumerKey = 'YGyvA1gPfP4kLgEAK5OYmvqLQ'
 const consumerSecret = 'A9NNp4y5TL9GyTsjwmPzuTxDGrxdpf8gftMk0SVnsWS6npEOBh'
-const callbackUrl = 'http://seayang.me/serivce/twitter/token'
+const callbackUrl = 'https://seayang.me/serivce/twitter/token'
 const sessionConfig = {
   key: 'koa:sess',
   maxAge: 86400000
