3d291ceaefbd68daedf73e66b1b4072db68a4a75
seayang <ykudos@gmail.com>
2018-02-28 14:10:36 +0800

    update router

--- app.js
+++ app.js
@@ -8,16 +8,18 @@ import { requestToken, getToken, verifyCredentials, queryTimeline, postTweeting 
 import authorize from './middlewares/authorize'
 
 const app = new Koa()
-const router = new Router()
+const router = new Router({
+  prefix: '/service/twitter'
+})
 
 app.keys = ['twitter', 'SetrlhpTdCE5WjJhdm']
 app.use(session(sessionConfig, app))
 app.use(bodyParser())
-router.get('/twitter/request/token', requestToken)
-router.get('/twitter/token', getToken)
-router.get('/twitter/credentials', authorize, verifyCredentials)
-router.get('/twitter/timeline', authorize, queryTimeline)
-router.post('/twitter/update', authorize, postTweeting)
+router.get('/request/token', requestToken)
+router.get('/token', getToken)
+router.get('/credentials', authorize, verifyCredentials)
+router.get('/timeline', authorize, queryTimeline)
+router.post('/update', authorize, postTweeting)
 app.use(router.routes()).use(router.allowedMethods())
 
 const port = process.env.PORT || '3000'
